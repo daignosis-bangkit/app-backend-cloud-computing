e7ab06a5c7bcd3c89a52f9dfc504baf5b00a3b75
ilhamm02 <ilhamaulana24@gmail.com>
2023-05-10 20:00:07 +0700

    Revert "backend for article page"
    This reverts commit b990be47f088fba07a3b610a49973276d0f6c1e3.

--- routers/userRouters.js
+++ routers/userRouters.js
@@ -1,20 +1,6 @@
 const express = require("express");
-const { userControllers  } = require("../controllers");
 const routers = express.Router();
-const Multer = require("multer");
-const imgUpload = require("../modules/imgUpload");
-
-const multer = Multer({
-  storage: Multer.MemoryStorage,
-  fileSize: 5 * 1024 * 1024,
-});
-
-routers.post(
-  "/profile",
-  multer.single("attachment"),
-  imgUpload.uploadToGcs,
-  userControllers.profile
-);
+const { userControllers } = require("../controllers");
 
 routers.post("/register", userControllers.register);
 
