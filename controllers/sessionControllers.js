2408f86d47555bb0b7da8479211ebb7b30ac09f5
ilhamm02 <ilhamaulana24@gmail.com>
2023-05-15 19:30:53 +0700

    backend for restore session

--- controllers/sessionControllers.js
+++ controllers/sessionControllers.js
@@ -1,8 +1,9 @@
-const uuid = require('uuid');
+const { read } = require("fs");
+const uuid = require("uuid");
 const { db } = require("../helper/configSql");
 
 module.exports = {
-	new: (socket, io, data) => {
+  new: (socket, io, data) => {
     if (typeof data !== "object") return false;
     if (!data.username) return false;
 
@@ -37,7 +38,30 @@ module.exports = {
       }
     );
   },
-	get: (res, req) => {
-		
-	}
-}
+  get: (req, res) => {
+    db.query(
+      `SELECT Session.session_id, Chat.message, Chat.is_bot, Chat.date AS latest_chat_date
+		FROM tbl_user User
+		JOIN tbl_session Session ON User.user_id = Session.user_id
+		JOIN tbl_chat Chat ON Session.session_id = Chat.session_id
+		JOIN (
+				SELECT session_id, MAX(date) AS max_date
+				FROM tbl_chat
+				GROUP BY session_id
+		) AS T ON Chat.session_id = T.session_id AND Chat.date = T.max_date
+		WHERE User.username = ?`,
+      [req.user.username],
+      (err, result) => {
+        if (err)
+          return res.status(500).send({
+            message: "Internal server error",
+            error: err.message,
+          });
+
+        return res.status(200).send({
+          data: result,
+        });
+      }
+    );
+  },
+};
